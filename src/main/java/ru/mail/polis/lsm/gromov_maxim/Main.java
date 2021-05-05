1a0532e5accaabdb77b161bbc4a391cae088b494
Maxim-Byte <maxim5gromov@gmail.com>
2021-05-05 14:48:59 +0300

    Gromov Maxim : stage1

---
+++ src/main/java/ru/mail/polis/lsm/gromov_maxim/Main.java
@@ -0,0 +1,10 @@
+package ru.mail.polis.lsm.gromov_maxim;
+
+import ru.mail.polis.lsm.DAOConfig;
+import ru.mail.polis.lsm.DAOFactory;
+
+public class Main {
+    public static void main(String[] args) {
+        // do something
+    }
+}
