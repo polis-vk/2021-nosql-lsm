15b99933b1d0588c30b3d6fba6b0c1bfe0c00dd2
OGSegu <krivitskij.vv@edu.spbstu.ru>
2021-05-07 13:57:26 +0300

    Created private DAOFactory constructor

--- src/main/java/ru/mail/polis/lsm/DAOFactory.java
+++ src/main/java/ru/mail/polis/lsm/DAOFactory.java
@@ -4,6 +4,9 @@ import java.io.IOException;
 
 public class DAOFactory {
 
+    private DAOFactory() {
+    }
+
     public static DAO create(DAOConfig config) throws IOException {
         throw new UnsupportedOperationException("Implement me");
     }
