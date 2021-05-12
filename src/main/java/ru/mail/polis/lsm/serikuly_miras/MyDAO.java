48bac7ba00415ffeb482d56a6998941e6c21b04d
Miras <71659835+Serikuly-Miras@users.noreply.github.com>
2021-05-12 17:43:00 +0300

    Using ConcurrentSkipListMap instead od TreeMap

---
+++ src/main/java/ru/mail/polis/lsm/serikuly_miras/MyDAO.java
@@ -0,0 +1,51 @@
+package ru.mail.polis.lsm.serikuly_miras;
+
+import ru.mail.polis.lsm.DAO;
+import ru.mail.polis.lsm.DAOConfig;
+import ru.mail.polis.lsm.Record;
+
+import javax.annotation.Nullable;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.Iterator;
+import java.util.SortedMap;
+import java.util.concurrent.ConcurrentSkipListMap;
+
+public class MyDAO implements DAO {
+    private final DAOConfig config;
+    private final SortedMap<ByteBuffer, Record> storage = new ConcurrentSkipListMap<>();
+
+    public MyDAO(DAOConfig config) {
+        this.config = config;
+    }
+
+    @Override
+    public Iterator<Record> range(@Nullable ByteBuffer fromKey, @Nullable ByteBuffer toKey) {
+        return subMap(fromKey, toKey).values().iterator();
+    }
+
+    @Override
+    public void upsert(Record record) {
+        if (record.getValue() == null) {
+            storage.remove(record.getKey());
+        } else {
+            storage.put(record.getKey(), record);
+        }
+    }
+
+    @Override
+    public void close() throws IOException {
+        // Do nothing
+    }
+
+    private SortedMap<ByteBuffer, Record> subMap(@Nullable ByteBuffer fromKey, @Nullable ByteBuffer toKey) {
+        if (fromKey == null && toKey == null) {
+            return storage;
+        } else if (fromKey == null) {
+            return storage.headMap(toKey);
+        } else if (toKey == null) {
+            return storage.tailMap(fromKey);
+        }
+        return storage.subMap(fromKey, toKey);
+    }
+}
---
+++ src/main/java/ru/mail/polis/lsm/serikuly_miras/package-info.java
@@ -0,0 +1,4 @@
+@ParametersAreNonnullByDefault
+package ru.mail.polis.lsm.serikuly_miras;
+
+import javax.annotation.ParametersAreNonnullByDefault;
