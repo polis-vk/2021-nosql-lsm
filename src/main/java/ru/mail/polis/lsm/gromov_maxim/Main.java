e4a1c29da9903bcd7ab9afe1006f36a2f095dbfa
Maxim-Byte <maxim5gromov@gmail.com>
2021-05-12 17:58:06 +0300

    Build fix Gromov Maxim stage 1

--- src/main/java/ru/mail/polis/lsm/gromov_maxim/Main.java
+++ src/main/java/ru/mail/polis/lsm/gromov_maxim/Main.java
@@ -3,9 +3,6 @@ package ru.mail.polis.lsm.gromov_maxim;
 import java.io.IOException;
 
 public class Main {
-    private void test() throws IOException, InterruptedException {
-        System.out.println("заглушка");
-    }
 
     public static void main(String[] args) throws IOException, InterruptedException {
         final Benchmark benchmark = new Benchmark();
