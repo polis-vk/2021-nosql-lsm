c7bb389642201bbd9386f065469399de35f0cce5
ogamoga <cupkek228@gmail.com>
2021-05-24 06:13:36 +0300

    implement stage3 with n-way merge

--- src/main/java/ru/mail/polis/lsm/DAO.java
+++ src/main/java/ru/mail/polis/lsm/DAO.java
@@ -3,8 +3,10 @@ package ru.mail.polis.lsm;
 import javax.annotation.Nullable;
 import java.io.Closeable;
 import java.nio.ByteBuffer;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.PriorityQueue;
 
 /**
  * Minimal database API.
@@ -35,7 +37,62 @@ public interface DAO extends Closeable {
     }
 
     static Iterator<Record> merge(List<Iterator<Record>> iterators) {
-        throw new UnsupportedOperationException("Implement me");
+
+        class QueueUnit implements Comparable<QueueUnit> {
+            private final Record data;
+            private final int source;
+
+            public QueueUnit(Record data, int source) {
+                this.data = data;
+                this.source = source;
+            }
+
+
+            public Record getData() {
+                return data;
+            }
+
+            public int getSourceNumber() {
+                return source;
+            }
+
+            @Override
+            public int compareTo(QueueUnit o) {
+                int keyCompare = data.getKey().compareTo(o.getData().getKey());
+                if (keyCompare == 0) {
+                    return o.getSourceNumber() - source;
+                }
+                return keyCompare;
+            }
+        }
+
+        PriorityQueue<QueueUnit> queue = new PriorityQueue<>();
+
+        for (int iterNumber = 0; iterNumber < iterators.size(); iterNumber++) {
+            Iterator<Record> iterator = iterators.get(iterNumber);
+            if (iterator.hasNext()) {
+                queue.add(new QueueUnit(iterator.next(), iterNumber));
+            }
+        }
+
+        ArrayList<Record> result = new ArrayList<Record>();
+
+        while (!queue.isEmpty()) {
+            QueueUnit current = queue.poll();
+
+
+            Iterator<Record> currentIter = iterators.get(current.getSourceNumber());
+            if (result.isEmpty() || !current.getData().getKey().equals(result.get(result.size() - 1).getKey())) {
+                result.add(current.getData());
+            }
+
+            if (currentIter.hasNext()) {
+                queue.add(new QueueUnit(currentIter.next(), current.getSourceNumber()));
+            }
+
+        }
+
+        return result.iterator();
     }
 
 }
--- src/main/java/ru/mail/polis/lsm/DaoImpl.java
+++ src/main/java/ru/mail/polis/lsm/DaoImpl.java
@@ -2,11 +2,15 @@ package ru.mail.polis.lsm;
 
 import javax.annotation.Nullable;
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.nio.ByteBuffer;
+import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
 import java.nio.channels.WritableByteChannel;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 import java.nio.file.StandardOpenOption;
 import java.util.Iterator;
 import java.util.NavigableMap;
@@ -14,24 +18,60 @@ import java.util.SortedMap;
 import java.util.concurrent.ConcurrentSkipListMap;
 
 public class DaoImpl implements DAO {
+
+    private static final Method CLEAN;
+
+
+    static {
+        try {
+            Class<?> aClass = Class.forName("sun.nio.ch.FileChannelImpl");
+            CLEAN = aClass.getDeclaredMethod("unmap", MappedByteBuffer.class);
+            CLEAN.setAccessible(true);
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
     private final DAOConfig config;
     private final NavigableMap<ByteBuffer, Record> map;
-    private static final String SAVE_FILE_NAME = "save.dat";
+    private final Path saveFileName;
+    private final Path tmpFileName;
 
+    private final MappedByteBuffer mmap;
+
+    /**
+     * Implementation of DAO with Persistence.
+     */
     public DaoImpl(DAOConfig config) throws IOException {
         this.config = config;
         this.map = new ConcurrentSkipListMap<>();
-        Path resolve = config.getDir().resolve(SAVE_FILE_NAME);
-        if (Files.exists(resolve)) {
-            try (FileChannel fileChannel = FileChannel.open(resolve, StandardOpenOption.READ)) {
-               ByteBuffer buffer = ByteBuffer.allocate(Integer.BYTES);
-                while (fileChannel.position() < fileChannel.size()) {
-                    ByteBuffer key = readBuffer(fileChannel, buffer);
 
-                    ByteBuffer value = readBuffer(fileChannel, buffer);
+        Path dir = config.getDir();
+        saveFileName = dir.resolve("save.dat");
+        tmpFileName = dir.resolve("tmp.dat");
 
-                    map.put(key, new Record(key, value));
-                }
+        if (!Files.exists(saveFileName)) {
+            if (Files.exists(tmpFileName)) {
+                Files.move(tmpFileName, saveFileName, StandardCopyOption.ATOMIC_MOVE);
+            } else {
+                mmap = null;
+                return;
+            }
+        }
+        try (FileChannel fileChannel = FileChannel.open(saveFileName, StandardOpenOption.READ)) {
+            mmap = fileChannel.map(FileChannel.MapMode.READ_ONLY, 0, fileChannel.size());
+            while (mmap.hasRemaining()) {
+                int keySize = mmap.getInt();
+                ByteBuffer key = mmap.slice().limit(keySize).asReadOnlyBuffer();
+
+                mmap.position(mmap.position() + keySize);
+
+                int valueSize = mmap.getInt();
+                ByteBuffer value = mmap.slice().limit(valueSize).asReadOnlyBuffer();
+
+                mmap.position(mmap.position() + valueSize);
+
+                map.put(key, Record.of(key, value));
             }
         }
     }
@@ -40,7 +80,7 @@ public class DaoImpl implements DAO {
         buffer.position(0);
         fileChannel.read(buffer);
         buffer.position(0);
-        ByteBuffer tmp = ByteBuffer.allocate(buffer.getInt());
+        final ByteBuffer tmp = ByteBuffer.allocate(buffer.getInt());
         fileChannel.read(tmp);
         tmp.position(0);
         return tmp;
@@ -76,17 +116,32 @@ public class DaoImpl implements DAO {
 
     @Override
     public void close() throws IOException {
-        Files.deleteIfExists(config.getDir().resolve(SAVE_FILE_NAME));
-        Path file = config.getDir().resolve(SAVE_FILE_NAME);
-        try (FileChannel fileChannel = FileChannel.open(file, StandardOpenOption.WRITE, StandardOpenOption.CREATE_NEW)) {
+        try (FileChannel fileChannel = FileChannel.open(
+                tmpFileName,
+                StandardOpenOption.CREATE_NEW,
+                StandardOpenOption.WRITE,
+                StandardOpenOption.TRUNCATE_EXISTING
+        )) {
             ByteBuffer size = ByteBuffer.allocate(Integer.BYTES);
             for (Record record : map.values()) {
-                if (record.getValue() != null) {
+                if (!record.isTombstone()) {
                     writeInt(record.getKey(), fileChannel, size);
                     writeInt(record.getValue(), fileChannel, size);
                 }
             }
+            fileChannel.force(false);
         }
+
+        if (mmap != null) {
+            try {
+                CLEAN.invoke(null, mmap);
+            } catch (IllegalAccessException | InvocationTargetException e) {
+                throw new IOException(e);
+            }
+        }
+
+        Files.deleteIfExists(saveFileName);
+        Files.move(tmpFileName, saveFileName, StandardCopyOption.ATOMIC_MOVE);
     }
 
     private static void writeInt(ByteBuffer value, WritableByteChannel channel, ByteBuffer tmp) throws IOException {
