b6f0e47ee59eb89797bb1f4ebdaa6aec0ac59941
IgorSamohin <igor04012001@yandex.ru>
2021-05-29 03:46:45 +0300

    Add first attempt

--- src/main/java/ru/mail/polis/lsm/DAO.java
+++ src/main/java/ru/mail/polis/lsm/DAO.java
@@ -1,10 +1,15 @@
 package ru.mail.polis.lsm;
 
 import javax.annotation.Nullable;
+
 import java.io.Closeable;
 import java.nio.ByteBuffer;
+import java.nio.charset.CharacterCodingException;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.PriorityQueue;
 
 /**
  * Minimal database API.
@@ -34,8 +39,57 @@ public interface DAO extends Closeable {
         return result;
     }
 
+    class Entry {
+        Iterator<Record> iterator;
+        Record prevRecord;
+        int k;
+
+        public Entry(Iterator<Record> iterator, Record prevRecord, int k) {
+            this.iterator = iterator;
+            this.prevRecord = prevRecord;
+            this.k = k;
+        }
+    }
+
+
     static Iterator<Record> merge(List<Iterator<Record>> iterators) {
-        throw new UnsupportedOperationException("Implement me");
+        PriorityQueue<Entry> queue = new PriorityQueue<>((a, b) -> {
+            int i = a.prevRecord.getKey().compareTo(b.prevRecord.getKey());
+            if (i == 0) {
+                return a.k > b.k ? -1 : 1;
+            }
+
+            return i;
+        });
+
+        for (int i = 0; i < iterators.size(); i++) {
+            Iterator<Record> it = iterators.get(i);
+            if (it.hasNext()) {
+                queue.add(new Entry(it, it.next(), i));
+            }
+        }
+
+        List<Record> returnList = new ArrayList<>();
+        while (!queue.isEmpty()) {
+            Entry poll = queue.poll();
+
+            while (!queue.isEmpty() && queue.peek().prevRecord.getKey().compareTo(poll.prevRecord.getKey()) == 0) {
+                Entry head = queue.poll();
+
+                if (head != null && head.iterator.hasNext()) {
+                    head.iterator.next();
+                    queue.add(head);
+                }
+            }
+
+            returnList.add(poll.prevRecord);
+            if (poll.iterator.hasNext()) {
+                poll.prevRecord = poll.iterator.next();
+                queue.add(poll);
+            }
+        }
+
+        return returnList.subList(0, returnList.size()).iterator();
     }
 
 }
