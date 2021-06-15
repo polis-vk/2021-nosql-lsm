0169a76217758a7b514e7dd587a384858a26db90
IlyaAAAA <ilya-sachuk@mail.ru>
2021-06-15 21:17:54 +0300

    deleted unused import

--- src/main/java/ru/mail/polis/lsm/sachuk/ilya/DaoImpl.java
+++ src/main/java/ru/mail/polis/lsm/sachuk/ilya/DaoImpl.java
@@ -14,7 +14,6 @@ import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.NoSuchElementException;
 import java.util.SortedMap;
 import java.util.Spliterator;
 import java.util.Spliterators;
